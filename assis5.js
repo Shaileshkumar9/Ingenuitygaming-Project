c853e195040d22b3f6b69a60bfdd66f72746e87a
Shaileshkumar9 <shailesh.kumar_cs19@gla.ac.in>
2022-08-09 15:31:34 +0530

    assinment6

---
+++ assis5.js
@@ -0,0 +1,34 @@
+
+// document.body.style.backgroundColor='red';
+// var name=prompt("Enter your Name");
+// var a=prompt("Enter the First Number");
+// var b=prompt("Enter the Second Number");
+
+// var today = new Date();
+// var day = today.getDay();
+// var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+// //   var date = today.getFullYear()+'/'+(today.getMonth()+01)+'/'+today.getDate();
+// var date = "0"+today.getDate()+'/'+ ('0'+(today.getMonth()+01))+'/'+today.getFullYear();
+
+// //var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// var dateTime = date;
+
+// document.getElementById("date").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+
+
+
+// var add=Number(a) + Number(b);
+// var divide=a/b;
+// var mult=a*b;
+
+// var minu=a-b;
+
+//  document.getElementById("name3").innerHTML=name;
+// document.getElementById("add").innerHTML=add;
+// document.getElementById("divi").innerHTML=divide;
+// document.getElementById("mul").innerHTML=mult;
+// document.getElementById("sub").innerHTML=minu;
+console.log("Shailesh Yadav");
+console.log(5+6);
+// document.getElementById("date").innerHTML=date2;
+// document.getElementById("day").innerHTML=d
