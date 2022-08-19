2b327694a3c64fcc3122c52f2427c58243872ca7
Shaileshkumar9 <shailesh.kumar_cs19@gla.ac.in>
2022-08-19 16:23:06 +0530

    done

--- game.js
+++ game.js
@@ -1,62 +1,98 @@
 //DOM selector
-var h1=document.querySelector('h1');
-var startermole=document.querySelector('#mole');
-var starterdirt=document.querySelector('#mud');
-var startbtn=document.querySelector("#start");
-var points=document.querySelector(".points");
-var dirt=document.querySelectorAll(".dirt");
-var mole=document.querySelectorAll(".mole");
-var p=0;
+var h1 = document.querySelector("h1");
+var startermole = document.querySelector("#mole");
+var starterdirt = document.querySelector("#mud");
+var startbtn = document.querySelector("#start");
+var points = document.querySelector(".points");
+var dirt = document.querySelectorAll(".dirt");
+var mole = document.querySelectorAll(".mole");
+var points2 = document.querySelector(".points2");
+var p = 0;
+
+function start() {
+  var pp = 60000;
+
+  h1.style.visibility = "hidden";
+  startermole.style.visibility = "hidden";
+  starterdirt.style.visibility = "hidden";
+  startbtn.style.visibility = "hidden";
+  points.style.visibility = "visible";
+  points2.style.visibility = "visible";
+  dirt[0].style.visibility = "visible";
+  for (var i = 0; i < 6; i++) {
+    dirt[i].style.visibility = "visible";
+  }
+  
+  startti();
+
+  setInterval(visi, 1000);
+
+  function visi() {
+    var random = Math.floor(Math.random() * 6);
+    mole[random].style.visibility = "visible";
+
+    setTimeout(hidi, 800);
+
+    function hidi() {
+      mole[random].style.visibility = "hidden";
+    }
+  }
+  setTimeout(result, pp);
+
+  function result() {
+    if (pp == 60000) {
+      console.log("result");
+      console.log(p);
+      localStorage.setItem("point", p);
+      window.location.href = "gameEnd.html";
+    }
+  }
 
 
-function start()
-{
-    h1.style.visibility="hidden";
-    startermole.style.visibility="hidden";
-    starterdirt.style.visibility="hidden";
-    startbtn.style.visibility="hidden";
-    points.style.visibility="visible";
-    dirt[0].style.visibility="visible";
-     for(var i=0;i<6;i++)
-     {
-        dirt[i].style.visibility="visible";   
-     }
-    //  for(var j=0;j<6;j++)
-    //  {
-    //  const random = Math.floor(Math.random() * 6) + 1;
-     
-    //  mole[random].style.visibility="visible";
-     
-    //  mole[random].style.visibility="hidden";
+  //jkhfdkjvhdkj
 
-    //  console.log(random);
-    //  }
-   //  mole[0].style.visibility="visible";
-   setInterval(visi,1000);
+    // startti();
+}
+function point() {
+  p = p + 1;
+  document.getElementById("score").innerHTML = p;
+}
+function play() {
+  var audio = new Audio("./images/Powerful-Trap-.mp3");
+  audio.play();
+}
 
-   function visi()
-   {
-    var random = Math.floor(Math.random() * 6) ;
-    mole[random].style.visibility="visible";
-     mole[random].style.visibility="visible";
-    
 
-    setTimeout(hidi,800);
-   
-   function hidi()
-   {
-       mole[random].style.visibility="hidden";
-   }
-    
-   }
 
-   
-   
 
+var timer; 
+var timeLeft = 60; 
+function gameOver() {
+ 
+  cancelInterval(timer);
+  
+ 
+  //$('#playAgainButton').show();
 
 }
-function point()
-{
-   p=p+1;
-   document.getElementById('score').innerHTML=p;
+
+function updateTimer() {
+  timeLeft = timeLeft - 1;
+  if(timeLeft >= 0)
+    //$('#score').html(timeLeft);
+    document.getElementById('score2').innerHTML=timeLeft;
+  else {
+    gameOver();
+  }
+}
+
+function startti() {
+
+  timer = setInterval(updateTimer, 1000);
+  
+ 
+  updateTimer();
+  
+  
+   //$('#playAgainButton').hide();
 }
